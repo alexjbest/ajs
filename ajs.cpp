f84ef2134e666e58e7063aa2c1e400c97d525514
Alex J. Best <alex.j.best@gmail.com>
2016-02-16 15:05:45 +0000

    refactored a lot, now saves function as a list of lines which can be permuted

--- ajs.cpp
+++ ajs.cpp
@@ -1,8 +1,8 @@
 #include <asmjit/asmjit.h>
 #include <iostream>
 #include <fstream>
-#include <algorithm> 
-#include <functional> 
+#include <algorithm>
+#include <functional>
 #include <cctype>
 #include <locale>
 #include <sstream>
@@ -19,6 +19,7 @@ struct line {
   uint32_t instruction;
   asmjit::Operand ops[4];
   string label;
+  uint32_t align;
 };
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
@@ -53,13 +54,11 @@ static inline std::string &trim(std::string &s) {
   return ltrim(rtrim(s));
 }
 
-Imm getValAsImm(string val) // TODO deal with hex etc.
-{
+Imm getValAsImm(string val) { // TODO deal with hex etc.
   return Imm(std::strtoll(val.c_str(), NULL, 10));
 }
 
-X86GpReg getGpRegFromName(string name)
-{
+X86GpReg getGpRegFromName(string name) {
   if (name == "rdi") return rdi;
   if (name == "rsi") return rsi;
   if (name == "rdx") return rdx;
@@ -72,8 +71,7 @@ X86GpReg getGpRegFromName(string name)
   return rax;
 }
 
-X86Reg getRegFromName(string name) // TODO see if we can do this with asmjit magic
-{
+X86Reg getRegFromName(string name) { // TODO see if we can do this with asmjit magic
   if (name.at(0) == 'r')
     return getGpRegFromName(name);
   if (name == "xmm0") return xmm0;
@@ -103,9 +101,9 @@ X86Mem getPtrFromAddress(string addr)
     else
       scalar = 1;
     uint32_t shift =
-      (scalar == 1) ? 0 : 
-      (scalar == 2) ? 1 : 
-      (scalar == 4) ? 2 : 
+      (scalar == 1) ? 0 :
+      (scalar == 2) ? 1 :
+      (scalar == 4) ? 2 :
       (scalar == 8) ? 3 : -1;
     if (trim(bis[0]).length() == 0) // no base register TODO
       return ptr(base, index, shift, disp);
@@ -154,9 +152,7 @@ void loadFuncFromFile(list<line>& func, X86Assembler& a, char* file)
     if (*parsed[0].rbegin() == ':')
     {
       string label = parsed[0].substr(0, parsed[0].size() - 1);
-      if (labels.count(label) == 0)
-        labels[label] = a.newLabel();
-      a.bind(labels[label]);
+      func.insert(func.end(), (line){0, ops[0], ops[1], ops[2], ops[3], label, 0});
 
       parsed.erase(parsed.begin());
     }
@@ -165,7 +161,7 @@ void loadFuncFromFile(list<line>& func, X86Assembler& a, char* file)
       if (parsed[0] == ".align")
       {
         std::vector<std::string> args = split(parsed[1], ',');
-        a.align(kAlignCode, strtol(trim(args[0]).c_str(), NULL, 10));
+        func.insert(func.end(), (line){0, ops[0], ops[1], ops[2], ops[3], "", strtol(trim(args[0]).c_str(), NULL, 10)});
       }
       continue; // TODO are there any more common directives that affect performance?
     }
@@ -203,26 +199,113 @@ void loadFuncFromFile(list<line>& func, X86Assembler& a, char* file)
     }
     for (; i < 4; i++)
       ops[i] = Operand();
-    a.emit(X86Util::getInstIdByName(parsed[0].c_str()), ops[0], ops[1], ops[2], ops[3]);
-    func.insert(func.end(), (line){X86Util::getInstIdByName(parsed[0].c_str()), ops[0], ops[1], ops[2], ops[3], ""});
+    func.insert(func.end(), (line){X86Util::getInstIdByName(parsed[0].c_str()), ops[0], ops[1], ops[2], ops[3], "", 0});
   }
 }
 
-void superOptimise(list<line>& func, const int from, const int to)
+uint64_t callFunc(void* funcPtr, JitRuntime& runtime)
 {
-  std::list<line>::iterator it2 = func.begin();
-  std::list<line>::iterator it1 = it2++;
-  std::list<line>::iterator e = func.end();
-  for (;;)
+  // In order to run 'funcPtr' it has to be casted to the desired type.
+  // Typedef is a recommended and safe way to create a function-type.
+  typedef int (*FuncType)(uint64_t*, uint64_t*, uint64_t*, uint64_t);
+
+  // Using asmjit_cast is purely optional, it's basically a C-style cast
+  // that tries to make it visible that a function-type is returned.
+  FuncType callableFunc = asmjit_cast<FuncType>(funcPtr);
+
+  uint64_t cycles_high, cycles_high1, cycles_low, cycles_low1;
+  uint64_t o, b, c, start, end;
+  o = 0;
+  b = 8;
+  c = 24;
+
+  /*asm volatile (
+      "CPUID\n\t"
+      "RDTSC\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t": "=r" (cycles_high), "=r"
+      (cycles_low):: "%rax", "%rbx", "%rcx", "%rdx");*/
+
+  uint64_t z = callableFunc(&o, &b, &c, 1);
+
+  // end timing
+  /*asm volatile(
+      "RDTSCP\n\t"
+      "mov %%edx, %0\n\t"
+      "mov %%eax, %1\n\t": "=r" (cycles_high1), "=r"
+      "CPUID\n\t"
+      (cycles_low1):: "%rax", "%rbx", "%rcx", "%rdx");*/
+
+  start = ( (cycles_high << 32) | cycles_low );
+  end = ( (cycles_high1 << 32) | cycles_low1 );
+
+  //printf("z=%d\n", z);
+  //printf("o=%lu\n", o); // Outputs "o=8" for and_n.
+
+  runtime.release((void*)callableFunc);
+
+  return end - start;
+}
+
+uint64_t timeFunc(list<line>& func, X86Assembler& a, JitRuntime& runtime)
+{
+  for (list<line>::const_iterator ci = func.begin(); ci != func.end(); ++ci)
   {
-    std::swap(*it1, *it2);
-    it1 = it2++;
-    if (it2 == e)
-      return;
-    it1 = it2++;
-    if (it2 == e)
-      return;
+    line curLine = *ci;
+    if (curLine.align != 0)
+    {
+      a.align(kAlignCode, curLine.align);
+      continue;
+    }
+    if (curLine.label != "")
+    {
+      //if (labels.count(label) == 0)
+      //  labels[label] = a.newLabel();
+      //a.bind(labels[label]);
+    }
+    a.emit(curLine.instruction, curLine.ops[0], curLine.ops[1], curLine.ops[2], curLine.ops[3]);
   }
+
+  void* funcPtr = a.make();
+
+  // cout << "make successful" << endl;
+
+  return callFunc(funcPtr, runtime);
+}
+
+list<line> superOptimise(list<line>& func, X86Assembler& a, JitRuntime& runtime, const int from, const int to)
+{
+  uint64_t bestTime = -1;
+  list<line> bestFunc;
+
+  for (int i = 0; i < 100; i++) // TODO run over all perms between from and to
+  {
+    // time this permutation
+    uint64_t newTime = timeFunc(func, a, runtime);
+    if (newTime < bestTime)
+    {
+      bestFunc = func;
+      bestTime = newTime;
+      cout << "more optimal sequence found: " << newTime << endl;
+    }
+
+    // make next permutation
+    std::list<line>::iterator it2 = func.begin();
+    std::list<line>::iterator it1 = it2++;
+    std::list<line>::iterator e = func.end();
+    for (;;)
+    {
+      std::swap(*it1, *it2);
+      it1 = it2++;
+      if (it2 == e)
+        break;
+      it1 = it2++;
+      if (it2 == e)
+        break;
+    }
+  }
+
+  return bestFunc;
 }
 
 int main(int argc, char* argv[]) {
@@ -237,52 +320,22 @@ int main(int argc, char* argv[]) {
   // Create JitRuntime and X86 Assembler/Compiler.
   JitRuntime runtime;
   X86Assembler a(&runtime);
-  a.setLogger(&logger);
 
-  list<line> func;
+  // Create the function we will work with
+  list<line> func, bestFunc;
 
+  // load it from the file given in arguments
   loadFuncFromFile(func, a, argv[1]);
-  for (list<line>::const_iterator ci = func.begin(); ci != func.end(); ++ci)
-    cout << ci->instruction << endl;
-  superOptimise(func, 2, 3);
 
-  cout << "optimisation complete" << endl;
+  cout << "original sequence:" << endl;
   for (list<line>::const_iterator ci = func.begin(); ci != func.end(); ++ci)
     cout << ci->instruction << endl;
 
-  // After finalization the code has been send to `Assembler`. It contains
-  // a handy method `make()`, which returns a pointer that points to the
-  // first byte of the generated code, which is the function entry in our
-  // case.
-  void* funcPtr = a.make();
+  bestFunc = superOptimise(func, a, runtime, 2, 3);
 
-  cout << "make successful" << endl;
-
-  // In order to run 'funcPtr' it has to be casted to the desired type.
-  // Typedef is a recommended and safe way to create a function-type.
-  typedef int (*FuncType)(uint64_t*, uint64_t*, uint64_t*, uint64_t);
-
-  // Using asmjit_cast is purely optional, it's basically a C-style cast
-  // that tries to make it visible that a function-type is returned.
-  FuncType callableFunc = asmjit_cast<FuncType>(funcPtr);
-
-  uint64_t o, b, c;
-  o = 0;
-  b = 8;
-  c = 24;
-  // Finally, run it and do something with the result...
-  int z = callableFunc(&o, &b, &c, 1);
-  printf("z=%d\n", z);
-  printf("o=%lu\n", o); // Outputs "o=8" for and_n.
-
-  // The function will remain in memory after Compiler and Assembler are
-  // destroyed. This is why the `JitRuntime` is used - it keeps track of
-  // the code generated. When `Runtime` is destroyed it also invalidates
-  // all code relocated by it (which is in our case also our `func`). So
-  // it's safe to just do nothing in our case, because destroying `Runtime`
-  // will free `func` as well, however, it's always better to release the
-  // generated code that is not needed anymore manually.
-  runtime.release((void*)callableFunc);
+  cout << "optimisation complete, best sequence found:" << endl;
+  for (list<line>::const_iterator ci = bestFunc.begin(); ci != bestFunc.end(); ++ci)
+    cout << ci->instruction << endl;
 
   return 0;
 }
