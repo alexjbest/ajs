62ba76800f30b582ddaf7b14710f4fff905693df
Alexander Kruppa <akruppa@gmail.com>
2016-08-31 14:16:31 +0200

    Better tests of split_sum()

--- unittests.cpp
+++ unittests.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <cassert>
+#include <cstdarg>
 #include "utils.h"
 
 using std::string;
@@ -36,34 +37,57 @@ void tests_skip_brackets(void)
     assert(test_skip_brackets("(())a", 3));
 }
 
-void
-test_split_sum(const char *t)
+bool
+test_split_sum(const char *t, const size_t num, ...)
 {
+    va_list va;
     string s = t;
     vector<string> tokens;
+    bool ok = true;
+
+    va_start(va, num);
+
     tokens = split_sum(s.begin(), s.end());
-    cout << "Input: " << t << endl;
-    for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); it++)
-        cout << "Token: " << *it << endl;
+    if (tokens.size() != num) {
+        ok = false;
+        cout << "Error, split_sum(" << s << ") returned " << tokens.size()
+                << " items, expected " << num << "\n";
+    }
+
+    // cout << "Input: " << t << endl;
+    for (vector<string>::iterator it = tokens.begin(); ok && it != tokens.end(); it++) {
+        const char *expected_token = va_arg (va, const char *);
+        if (*it != expected_token) {
+            ok = false;
+            cout << "Error, got token " << *it << " but expected "
+                    << expected_token << endl;
+        }
+    }
+
+    va_end(va);
+    return ok;
 }
 
-void tests_split_sum()
+bool tests_split_sum()
 {
-    test_split_sum("a");
-    test_split_sum("+");
-    test_split_sum("-");
+    return
+    test_split_sum("a", 1, "a") &&
+    // I'm not really sure what these cases should return, but we test
+    // that they at least return at all
+    test_split_sum("+", 1, "+") &&
+    test_split_sum("-", 1, "-") &&
 
-    test_split_sum("++");
-    test_split_sum("+a");
-    test_split_sum("a+");
-    test_split_sum("aa");
-    test_split_sum("-a");
-    test_split_sum("a-");
+    test_split_sum("++", 1, "+") &&
+    test_split_sum("+a", 1, "+a") &&
+    test_split_sum("a+", 1, "a") &&
+    test_split_sum("aa", 1, "aa") &&
+    test_split_sum("-a", 1, "-a") &&
+    test_split_sum("a-", 2, "a", "-") &&
 
-    test_split_sum("a+b");
-    test_split_sum("a-b");
-    test_split_sum("-a+b");
-    test_split_sum("-a-b");
+    test_split_sum("a+b", 2, "a", "b") &&
+    test_split_sum("a-b", 2, "a", "-b") &&
+    test_split_sum("-a+b", 2, "-a", "b") &&
+    test_split_sum("-a-b", 2, "-a", "-b");
 }
 
 int main()
