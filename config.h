90f84c55b363d5506dd72894c035f4582aa8dc0a
Alexander Kruppa <akruppa@gmail.com>
2016-09-06 18:05:59 +0200

    Use less warmup and LOOPSIZE of only 1

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 
 #define MAX_OPS 3
 #define TRIALS 600
-#define LOOPSIZE 4
-#define WARMUP_LENGTH 100000
+#define LOOPSIZE 1
+#define WARMUP_LENGTH 10000
 #define REPEATS 1
 #define PREFETCH_CALLS 4
 
