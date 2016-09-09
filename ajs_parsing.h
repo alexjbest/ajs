7a2cb7dfeddd349f8e653aa6286af3e0a3fe10cb
Alexander Kruppa <akruppa@gmail.com>
2016-09-09 18:19:17 +0200

    Refactor getGpRegFromName(), and allow "%axl" for "%al" as gas does

--- ajs_parsing.h
+++ ajs_parsing.h
@@ -11,7 +11,8 @@
 #include <string>
 #include <asmjit/asmjit.h>
 
-#define regreg(N)  do {if (name == #N) return N;} while(0)
+#define regreg2(N,M)  do {if (name == #N) return M;} while(0)
+#define regreg(N) regreg2(N,N)
 
 static asmjit::X86Reg getXmmRegFromName(std::string name) {
     using namespace asmjit::x86;
@@ -34,25 +35,51 @@ static asmjit::X86Reg getXmmRegFromName(std::string name) {
     return noGpReg;
 }
 
+/* The ax, bx, cx, dx registers can be named, e.g.,
+ * rax, eax, ax, ah, al, axl (=al)
+ * to refer to different widths/parts.
+ * The "axl" form is supported by gas, so we allow it, too. */
+#define regabcd(X) \
+    do { \
+        regreg(r##X##x); \
+        regreg(e##X##x); \
+        regreg(X##x); \
+        regreg(X##h); \
+        regreg(X##l); \
+        regreg2(X##xl,X##l); \
+    } while (0)
+
+/* The bp, sp, si, di registers can be named, e.g.,
+ * rbp, ebp, bp, bpl.
+ */
+
+#define regbssd(X) \
+    do { \
+        regreg(r##X); \
+        regreg(e##X); \
+        regreg(X); \
+        regreg(X##l); \
+    } while (0)
+
+/* The r8, ..., r15 registers can be named, e.g.,
+ * r8, r8d, r8w, r8b.
+ */
+
+#define reg8_15(X) \
+    do { \
+        regreg(r##X); \
+        regreg(r##X##d); \
+        regreg(r##X##w); \
+        regreg(r##X##b); \
+    } while (0)
+
 static inline asmjit::X86GpReg
 getGpRegFromName(std::string name) {
   using namespace asmjit::x86;
-  regreg(rax);  regreg(eax);   regreg(ax);   regreg(ah);  regreg(al);
-  regreg(rbx);  regreg(ebx);   regreg(bx);   regreg(bh);  regreg(bl);
-  regreg(rcx);  regreg(ecx);   regreg(cx);   regreg(ch);  regreg(cl);
-  regreg(rdx);  regreg(edx);   regreg(dx);   regreg(dh);  regreg(dl);
-  regreg(rbp);  regreg(ebp);   regreg(bp);
-  regreg(rsp);  regreg(esp);   regreg(sp);
-  regreg(rsi);  regreg(esi);   regreg(si);
-  regreg(rdi);  regreg(edi);   regreg(di);
-  regreg(r8);   regreg(r8d);   regreg(r8w);               regreg(r8b);
-  regreg(r9);   regreg(r9d);   regreg(r9w);               regreg(r9b);
-  regreg(r10);  regreg(r10d);  regreg(r10w);              regreg(r10b);
-  regreg(r11);  regreg(r11d);  regreg(r11w);              regreg(r11b);
-  regreg(r12);  regreg(r12d);  regreg(r12w);              regreg(r12b);
-  regreg(r13);  regreg(r13d);  regreg(r13w);              regreg(r13b);
-  regreg(r14);  regreg(r14d);  regreg(r14w);              regreg(r14b);
-  regreg(r15);  regreg(r15d);  regreg(r15w);              regreg(r15b);
+  regabcd(a);  regabcd(b);  regabcd(c);  regabcd(d);
+  regbssd(bp); regbssd(sp); regbssd(si); regbssd(di);
+  reg8_15(8);  reg8_15(9);  reg8_15(10); reg8_15(11);
+  reg8_15(12); reg8_15(13); reg8_15(14); reg8_15(15);
   return noGpReg;
 }
 
