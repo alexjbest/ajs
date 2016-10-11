56f5d03620ac2a18c39ac7466e8be7d14ba83d59
Alexander Kruppa <akruppa@gmail.com>
2016-10-11 17:36:01 +0200

    Set TIMING_SERIALIZE to 1 and SKIP_CONTEXT_SWITCHES to 1

--- config.h
+++ config.h
@@ -6,8 +6,8 @@
 #define WARMUP_LENGTH 100
 #define REPEATS 1
 #define PREFETCH_CALLS 8
-#define TIMING_SERIALIZE rdtscpl
-// #define SKIP_CONTEXT_SWITCHES
+#define TIMING_SERIALIZE 1
+#define SKIP_CONTEXT_SWITCHES 1
 
 //#define STRATEGY_MIN
 //#define USE_INTEL_PCM
